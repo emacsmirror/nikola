95f1f314e34f005058fb00be216c2f67e68efaba
drymer <drymer@autistici.org>
2016-12-29 21:35:14 +0100

    Add commentary

--- nikola.el
+++ nikola.el
@@ -1,23 +1,63 @@
 ;;; nikola.el --- Minor mode for managing a nikola site
-
 ;; Author:: drymer <drymer [ EN ] autistici.org>
 ;; Copyright:: Copyright (c) 2016, drymer
-
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 2 of the License, or (at
 ;; your option) any later version.
-
 ;; This program is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
-
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; nothing yet
+;; ◊ Main commands:
+;;   • `nikola-build': Builds the site.
+;;   • `nikola-start-webserver': Starts nikola's webserver.
+;;   • `nikola-stop-webserver': Stops nikola's webserver.
+;;   • `nikola-deploy': Deploys the site.
+;; ◊ Variables:
+;;   • `nikola-output-root-directory': Nikola's site directory.
+;;   • `nikola-verbose': If set to *t*, it will create a buffer called
+;;     **Nikola** with the output of all commands. Set to *nil* by default.
+;;   • `nikola-webserver-auto': If set to *t*, it will use `nikola auto' to
+;;     launch the webserver. If set to *nil*, it will use `nikola
+;;     serve'. Set to *nil* by default.
+;;   • `nikola-webserver-host': Set it to *0.0.0.0* if you want to make the
+;;     webserver accesible from outside the machine. Set to *127.0.0.1* by
+;;     default.
+;;   • `nikola-webserver-port': Nikola's webserver port. Set to *8000* by
+;;     default.
+;;   • `nikola-deploy-input': If *nil*, just execute plain deploy, if *t*,
+;;     asks for user input, *any string* is passed to the deploy string.
+;;     This variable is intended to use with a deploy script or command
+;;     that uses git, thus needs a commit message. It could be used for
+;;     whatever other reason, also. To use the message writed on emacs on
+;;     the deploy order, you have to use the variable *$COMMIT*. For
+;;     example:
+;;   ┌────
+;;   │ DEPLOY_COMMANDS = {
+;;   │      'default': [
+;;   │          "git add .", "git commit -m \"$COMMIT\"", "git push"
+;;   │      ]
+;;   │  }
+;;   └────
+;;   • `nikola-deploy-input-default': If `nikola-deploy-input' is *t*, this
+;;     variable changes the default value so you can just press RET.
+;; Hooks:
+;;   Use them as you would usually do.
+;;   • `nikola-build-before-hook'
+;;   • `nikola-build-after-hook'
+;;   • `nikola-deploy-before-hook'
+;;   • `nikola-deploy-after-hook'
+;;   If you only want to execute a simple script before or after building
+;;   or deploying, you can set the next variables to that script's path:
+;;   • `nikola-build-before-hook-script'
+;;   • `nikola-build-after-hook-script'
+;;   • `nikola-deploy-before-hook-script'
+;;   • `nikola-deploy-after-hook-script'
 
 ;;; Code:
 (defgroup nikola nil
